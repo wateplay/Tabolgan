26eda2b7fa92d9e13788da3e4502fc434a30a8b0
wateplay <brevikentir@gmail.com>
2019-07-14 14:41:07 +0700

    push

---
+++ java/com/example/iramml/clientapp/Messages/Errors.java
@@ -0,0 +1,8 @@
+package com.example.iramml.clientapp.Messages;
+
+public enum Errors {
+    ERROR_LOGIN_GOOGLE,
+    NOT_SUPPORT,
+    WITHOUT_LOCATION,
+    SENT_FAILED
+}
---
+++ java/com/example/iramml/clientapp/Messages/Messages.java
@@ -0,0 +1,7 @@
+package com.example.iramml.clientapp.Messages;
+
+public enum Messages {
+    PERMISSION_DENIED,
+    RATIONALE,
+    REQUEST_SUCCESS
+}
